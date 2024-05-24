64f0525ef148032cf662af64b534d8839b692549
Sumit <marvelsumit2002@gmail.com>
2024-05-24 20:56:07 +0530

    seventh try

--- Server/index.js
+++ Server/index.js
@@ -15,16 +15,11 @@ import './db.js'
 
 const app = express() 
  
-app.use(cors({
-    origin: ['https://bookstore-manage.vercel.app']
-}))
 app.use(express.json())
-
-
-// ({
-//     origin: [`https://bookstore-manage.vercel.app`],
-//     credentials: true
-// })
+app.use(cors({
+    origin: [`https://bookstore-interface.vercel.app`],
+    credentials: true
+}))
 app.use(cookieParser())
 
 app.use('/auth', adminRouter)
