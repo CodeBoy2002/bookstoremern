7cd06e8abb72f100c68b8b7a2687cd8bddf8f257
Sumit <marvelsumit2002@gmail.com>
2024-05-24 20:27:46 +0530

    third try

--- Server/index.js
+++ Server/index.js
@@ -17,7 +17,7 @@ const app = express()
  
 app.use(express.json())
 app.use(cors({
-    origin: [`https://bookstore-manage.vercel.app/`],
+    origin: [`https://bookstore-manage.vercel.app`],
     credentials: true
 }))
 app.use(cookieParser())
